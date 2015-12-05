1747e73a5fba97d4647b9a0a1cf92b377bbac4a7
Mark Sansait <sansaitm23@outlook.com>
2015-12-04 23:26:15 -0600

    Everything is good so far, but when i fiddle with contact page it does not load.

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,16 +1,34 @@
 //site.js
 
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Mark Sansait";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style["background-color"] = "#888";
-    };
+    //var ele = $("#username");
+    //ele.text("Mark Sansait");
 
-    main.onmouseleave = function () {
-        main.style["background-color"] = "";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style["background-color"] = "#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style["background-color"] = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () { 
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    })
 })();
 
