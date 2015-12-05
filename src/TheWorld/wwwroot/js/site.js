11a0b6090c54213ac166049d552a496d462a963d
Mark Sansait <sansaitm23@outlook.com>
2015-12-04 19:55:14 -0600

    added js functionality

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+//site.js
+
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Mark Sansait";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style["background-color"] = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style["background-color"] = "";
+    };
+})();
+
